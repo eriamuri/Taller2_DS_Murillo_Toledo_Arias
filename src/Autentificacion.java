b005dbc1b6d2c55e5ef2039ec26323f0e2c7c849
eriamuri <eriamuri@espol.edu.ec>
2024-10-16 22:09:43 -0500

    arreglos minimos

--- src/Autentificacion.java
+++ src/Autentificacion.java
@@ -3,5 +3,5 @@ public interface Autentificacion {
 }
 /*//Principio de Sustitución de Liskov (LSP): Clase LogInAdmin: Extiende de LogIn, 
 pero altera el comportamiento de la clase base al verificar si el usuario es administrador 
-y bloquear el acceso si no lo es por lo que se creó una interfaz que tenga la funcion log para que casa clase que extienda de ella lo configure a su gusto.
+y bloquear el acceso si no lo es por lo que se creó una interfaz que tenga la funcion log para que cada clase que extienda de ella lo configure a su gusto.
  */
